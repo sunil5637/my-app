b6a478baec91684f06727039675960dc5f0710e0
Sunil <jsunilkumarbairwa@gmail.com>
2023-10-08 13:49:19 +0530

    1st commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+
+  // template: '<h1>Hello World!</h1>',
+  // styles: ['h1 { font-size: 120px; }']
+
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+}
